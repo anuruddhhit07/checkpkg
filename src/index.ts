7ee938559be23806c2941717d006b343835c8e16
anuruddhhit07 <88274721+anuruddhhit07@users.noreply.github.com>
2024-05-02 16:20:30 +0900

    122344

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,8 @@
-import {add,add2} from "chartpakage"
+import {add,add2,intersection,drawLineChart} from "chartpakage"
 
 const sum1=add({a:2,b:3})
 console.log(sum1);
 
 const sum2=add2(3,4)
 console.log(sum2);
+console.log(intersection);
